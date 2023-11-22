4e027d800ca5f92b1b43447ccfe7211aa1d14c81
Stanislav <stanis1av.b@yandex.ru>
2023-11-22 16:07:28 +0300

    fix main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,6 @@ android {
 
 dependencies {
 
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/androidsoftwaremodules/MainActivity.kt
+++ app/src/main/java/com/example/androidsoftwaremodules/MainActivity.kt
@@ -1,46 +1,11 @@
 package com.example.androidsoftwaremodules
 
-import android.content.res.Resources
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.CompositePageTransformer
-import androidx.viewpager2.widget.MarginPageTransformer
-import androidx.viewpager2.widget.ViewPager2
-import java.lang.Math.abs
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        // Carousel Example
-        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
-
-        viewPager.apply {
-            clipChildren = false  // No clipping the left and right items
-            clipToPadding = false  // Show the viewpager in full width without clipping the padding
-            offscreenPageLimit = 3  // Render the left and right items
-            (getChildAt(0) as RecyclerView).overScrollMode =
-                RecyclerView.OVER_SCROLL_NEVER // Remove the scroll effect
-        }
-
-        val demoData = arrayListOf(
-            "Carousel, carousel begins the story",
-            "These are fairy tales, songs and fun!",
-            "Carousel, carousel is a joy for us",
-            "Take a ride on our carousel!."
-        )
-
-        viewPager.adapter = CarouselRVAdapter(demoData)
-
-        val compositePageTransformer = CompositePageTransformer()
-        compositePageTransformer.addTransformer(MarginPageTransformer((40 * Resources.getSystem().displayMetrics.density).toInt()))
-        compositePageTransformer.addTransformer { page, position ->
-            val r = 1 - abs(position)
-            page.scaleY = (0.80f + r * 0.20f)
-        }
-        viewPager.setPageTransformer(compositePageTransformer)
     }
 }
